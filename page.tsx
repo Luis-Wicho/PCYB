ba17d24fc40addf52f01e4981758b88695c966d4
Luis-Wicho <sanchezlopezluisangel07@gmail.com>
2026-04-29 17:46:37 -0600

    actualizacion8

--- page.tsx
+++ page.tsx
@@ -2,14 +2,15 @@ import { createClient } from '@/utils/supabase/server'
 import { cookies } from 'next/headers'
 
 export default async function Page() {
-  const cookieStore = await cookies()
-  const supabase = createClient(cookieStore)
+  // AGREGA EL AWAIT AQUÍ:
+  const supabase = await createClient(cookies())
 
+  // Ahora 'from' funcionará porque supabase ya no es una promesa
   const { data: todos } = await supabase.from('todos').select()
 
   return (
     <ul>
-      {todos?.map((todo) => (
+      {todos?.map((todo: any) => (
         <li key={todo.id}>{todo.name}</li>
       ))}
     </ul>
