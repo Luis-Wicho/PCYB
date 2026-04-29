fbc2ca182e4520c942752fc0299272e8493f76c9
Luis-Wicho <sanchezlopezluisangel07@gmail.com>
2026-04-29 16:45:38 -0600

    actualizacion2

--- app/Establishments/edit/[id]/page.tsx
+++ app/Establishments/edit/[id]/page.tsx
@@ -1,8 +1,5 @@
-<<<<<<< HEAD
+import EditEstablishmentsView from "@/modules/Establishments/presentation/views/editEstablishmentsView";
 import EditEstablishmentView from "@/modules/Establishments/presentation/views/editEstablishmentsView";
-=======
-import EditEstablishmentsView from "@/modules/Establishments/presentation/views/EditEstablishmentsView";
->>>>>>> a46c6275640228398b0bb8be1fc7644d3318c332
 
 export default function Page() {
   return <EditEstablishmentsView />;
--- app/rates/page.tsx
+++ app/rates/page.tsx
@@ -1,8 +1,5 @@
-<<<<<<< HEAD
 import RateFrom from "@/modules/RateManagement/presentation/views/RateFrom";
 
 export default function Page(){
    return <RateFrom/>
 }
-=======
->>>>>>> a46c6275640228398b0bb8be1fc7644d3318c332
